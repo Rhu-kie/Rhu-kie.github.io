2dd55fc6b7a6e355f1a4898a81fd1461f7692cb9
Okugbere Oghenerukevwe Kessiena <101344891+Rhu-kie@users.noreply.github.com>
2026-09-15 17:49:13 +0100

    Add custom domain rhukie.me

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rhu-kie.github.io',
+  site: 'https://rhukie.me',
   markdown: {
     // Design spec calls for no syntax highlighting, ink tints only via our
     // own CSS. Shiki's default theme applies inline styles (dark background,
